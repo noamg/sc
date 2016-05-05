ae0b1d71ebbbb460180d27793b86b6f695ec19c5
noam <gavishnoam@gmail.com>
2016-05-05 17:47:45 +0300

    RT_meas new date

--- matlabio/RT_Meas.m
+++ matlabio/RT_Meas.m
@@ -34,7 +34,7 @@ try
     
     pauseInterval=0.25; % Seconds
     
-    savePath='C:\dan_noam_sc.git\data\110416\';
+    savePath='C:\dan_noam_sc.git\data\050516\';
     fnamePattern='cooling_long';
     fname=nextAvailableFilename(savePath,fnamePattern,'csv');
     header='Time(sec),TempRes(Ohm),SampVolt(V),SampCurr(A),CoilCurr(A)\n';

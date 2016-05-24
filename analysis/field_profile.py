c7a5a0d2532b6bb1a3559679a58864f43818ee7e
noam <gavishnoam@gmail.com>
2016-05-24 12:37:34 +0300

    happy with wire field, with numeric avarage, not with analytical avarage

--- analysis/field_profile.py
+++ analysis/field_profile.py
@@ -7,6 +7,7 @@ Created on Fri Apr 15 16:27:39 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 #%%
 def f(x,z):
@@ -21,15 +22,35 @@ for z in range(1,8):
 plt.legend(loc='best')
 
 #%%
-def H_z(x_wire, z_wire, x, z, current, mu_0=4*np.pi*1e-7):
-    xx, zz = np.meshgrid(x, z)
-    Tesla_to_gauss = 1e4
-    return mu_0 * current / 2 / np.pi * Tesla_to_gauss * (xx - x_wire) / ((xx - x_wire) ** 2 + (zz - z_wire) ** 2)
+Tesla_to_gauss = 1e4
+def H_z_wire(x_wire, z_wire, xx, zz, current, mu_0=4*np.pi*1e-7):    
+    return mu_0 * current / 2 / np.pi * Tesla_to_gauss * (x_wire - xx) / ((xx - x_wire) ** 2 + (zz - z_wire) ** 2)
+
+def H_z_wire_avaraged(x_wire, z_wire, x, z_min, z_max, current, mu_0=4*np.pi*1e-7):
+    return mu_0 * current / 2 / np.pi * Tesla_to_gauss / (z_max - z_min) * (np.arctan(z_max / x) - np.arctan(z_min / x))
+    
     
 #%%
-    
-x=np.linspace(-1.5e-3, 1.5e-3, 10)
-z=np.linspace(-0.5e-3, 0.5e-3, 5)
-H = H_z(1.6e-3, 0, x, z, 4)
+
+N_x = 60
+N_z = 20
+x, dx = np.linspace(-1.5e-3, 1.5e-3, N_x, retstep=True)
+z, dz = np.linspace(-0.5e-3, 0.5e-3, N_z, retstep=True)
+xx, zz = np.meshgrid(x, z)
+#z = 0.1e-3
+H = 5 * H_z_wire(1.6e-3, 0, xx, zz, 4) + 6 * H_z_wire(-1.6e-3, 0, xx, zz, 4)
+H_a = 5 * H_z_wire_avaraged(1.6e-3, 0, x, z[0], z[-1], 4) + 6 * H_z_wire_avaraged(-1.6e-3, 0, x, z[0], z[-1], 4)
+H_a_numeric = np.sum(H, axis=0) * dz / (z[-1] - z[0])
+#plt.figure()
+#plt.plot(x, H[3, :])
+#plt.plot(x, H[0,:])
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_wireframe(xx, zz, H)
+
 plt.figure()
-plt.plot(x, H[3, :])
+plt.plot(x, H_a)
+plt.plot(x, H_a_numeric)
+
+#%%
